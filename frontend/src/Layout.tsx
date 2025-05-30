1ac42db2baa0e0d70b6935a605da9edce3e48078
Eugene Kim <simplyeugene94@gmail.com>
2025-05-29 22:44:05 -0400

    fix footer to be at bottom of page

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,8 +1,6 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar, items } from "@/components/sidebar";
 import { ModeToggle } from "./components/theme-provider/mode-toggle";
-import Markdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
 import { useLocation } from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/sonner";
 
@@ -12,31 +10,20 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">
+      <div className="flex flex-col h-screen w-screen">
         <header className="text-left flex">
           <SidebarTrigger /> | {""}
           {items.find((item) => item.url === location.pathname)?.title}
         </header>
-        {children}
-        <ModeToggle />
-        <Toaster />
-        <footer>
-          <Markdown
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              a(props) {
-                const { node, ...rest } = props;
-                return <a className="text-blue-400 underline" {...rest}></a>;
-              },
-            }}
-          >
-            {`
-<div style='text-align: center; color: #666;'>
-© 2024 Ballot Initiative Project | <a href='#'>Privacy Policy</a> | <a href='#'>Terms of Use</a>
-</div>`}
-          </Markdown>
+        <main className="w-full mb-auto">
+          {children}
+          <ModeToggle />
+          <Toaster />
+        </main>
+        <footer className="w-full centered">
+          © 2024 Ballot Initiative Project
         </footer>
-      </main>
+      </div>
     </SidebarProvider>
   );
 }
